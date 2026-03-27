922e45bf1022dddee20cafd370302baf628dfeb7
oddinggerman-stack <oddinggerman@gmail.com>
2026-03-27 13:01:24 +0100

    Create outputs.tf

---
+++ modules/storage/outputs.tf
@@ -0,0 +1,15 @@
+output "storage_account_id" {
+  value = azurerm_storage_account.main.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.main.name
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.main.primary_blob_endpoint
+}
+
+output "file_share_name" {
+  value = azurerm_storage_share.configs.name
+}
